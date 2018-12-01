5e8958c7d487acf092194fa6ef55709145904a53
UmJaeJeong <39511137+UmJaeJeong@users.noreply.github.com>
2018-12-02 00:23:26 +0900

    줄세우기2252
    위상정렬문제 풀다가 멈춤 백준사이트

---
+++ 위상정렬/줄세우기 2252.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+#define MAX 32001
+//두명의 키를 비교
+//일부 학생들의 키만 비교하여 줄세우기
+//M은 키를 비교한 횟수
+
+int n, inDegree[MAX], result[MAX];
+vector<int> a[MAX];
+
+void topolgysort() {
+	queue<int> q;
+	//진입 차수가 0인 노드를 큐에 삽입.
+	for (int i = 1; i <= n; i++) {
+		if (inDegree[i] == 0) {
+			q.push(i);
+		}
+	}
+	
+	//정렬이 완전히 수행되려면 정확히 n개의 노드를 방문한다.
+	for (int i = 1; i <= i; i++) {
+		int x = q.front();
+		q.pop();
+		result[i] = x;
+		for (int j = 0; j < a[x].size(); j++) {
+			int y = a[x][j];
+			//새롭게 진입차수가 0이 된 정점을 큐에 삽입
+			if (--inDegree[y] == 0) q.push(y);
+		}
+	}
+
+	for (int i = 1; i <= n; i++) {
+		cout << result[i];
+	}
+
+}
+
+int main() {
+
+
+	return 0;
+}
