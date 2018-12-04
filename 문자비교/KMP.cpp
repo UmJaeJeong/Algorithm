d5d9610702a15026f8c1db7e02fbfd5b9ef4f6ff
UmJaeJeong <39511137+UmJaeJeong@users.noreply.github.com>
2018-12-04 20:15:25 +0900

    같은 길이  함수 makeTable
    makeTable함수 완성 KMP함수 남음

--- 문자비교/KMP.cpp
+++ 문자비교/KMP.cpp
@@ -8,7 +8,7 @@ using namespace std;
 vector<int> makeTable(string pattern) {
 	int patternSize = pattern.size();
 	vector<int> table(patternSize, 0);
-	int j = 0; 
+	int j = 0;
 
 	for (int i = 1; i < patternSize; i++) {
 		while (j > 0 && pattern[i] != pattern[j]) {
@@ -33,7 +33,26 @@ void KMP(string parent, string pattern) {
 	}
 }
 
-int main() {
 
+vector<int> makeTable(string pattern) {
+	int patternSize = pattern.size();
+	vector<int> table(patternSize, 0);
+	int j = 0;
+	for (int i = 1; i < patternSize; i++) {
+		while (j > 0 && pattern[i] != pattern[j]) {
+			j = table[j - 1];
+		}
+		if (pattern[i] == pattern[j]) table[i] = ++j;
+	}
+	return table;
+}
+
+
+int main() {
+	string pattern = "abacaaba";
+	vector<int> table = makeTable(pattern);
+	for (int i = 0; i < table.size(); i++) {
+		cout << table[i];
+	}
 	return 0;
 }
