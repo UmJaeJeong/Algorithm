b29be709b8a6cafc2d7e53a0ce4cbe16c3b4d1e5
UmJaeJeong <39511137+UmJaeJeong@users.noreply.github.com>
2018-12-04 19:31:01 +0900

    KMP
    접두사와 접미사를 이용한 알고리즘   수정중

---
+++ 문자비교/KMP.cpp
@@ -0,0 +1,39 @@
+#include<vector>
+#include<iostream>
+using namespace std;
+//KMP알고리즘 ==> 대칭을 이용 
+//접두사와 접미사를 이용하여 시간복잡도를 줄이는 알고리즘
+
+//같은 접두사와 접미사를 찾음
+vector<int> makeTable(string pattern) {
+	int patternSize = pattern.size();
+	vector<int> table(patternSize, 0);
+	int j = 0; 
+
+	for (int i = 1; i < patternSize; i++) {
+		while (j > 0 && pattern[i] != pattern[j]) {
+			j = table[j - 1];
+		}
+		//동일한건 체크
+		if (pattern[i] == pattern[j]) {
+			table[i] = ++j;
+		}
+	}
+	return table;
+}
+
+void KMP(string parent, string pattern) {
+	vector<int> table = makeTable(pattern);
+	int parentSize = parent.size();
+	int patternSize = pattern.size();
+	int j = 0;
+
+	for (int i = 0; i < parentSize; i++) {
+
+	}
+}
+
+int main() {
+
+	return 0;
+}
