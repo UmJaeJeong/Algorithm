db2ac91df54517839c2bd281357ba728e83c2a8c
UmJaeJeong <39511137+UmJaeJeong@users.noreply.github.com>
2018-12-22 01:26:11 +0900

    BFS문제 풀고있는중
     .. DFS로 풀었으나 시간초과로 BFS 로 품

---
+++ BFS,DFS/보물섬 2589.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include<string>
+#include<queue>
+using namespace std;
+#define MAX 51
+
+char a[MAX][MAX];
+int c[MAX][MAX];
+int Max;
+int d1[4] = { 0,0,1,-1 };
+int d2[4] = { 1,-1,0,0 };
+int n, m;
+
+
+void bfs(int i, int j) {
+	queue<pair<int,int>> q;
+	q.push(make_pair(i, j));
+
+	while (!q.empty()) {
+		pair<int, int> k = q.front();
+		q.pop();
+		for (int x = 0; x < 4; x++) {
+			int nx = k.first + d1[x];
+			int ny = k.second + d2[x];
+
+			if (nx >= 0 && nx<n && ny >= 0 && ny<m) {
+				if (a[nx][ny] == 'L' && c[nx][ny]==0) {
+					q.push(make_pair(nx, ny));
+					c[nx][ny] = c[i][j] + 1;
+					if (c[nx][ny]>Max) Max = c[nx][ny];
+				}
+			}
+		}
+	}
+	
+}
+
+void init() {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			c[i][j] = 0;
+		}
+	}
+}
+
+int main() {
+	cin >> n>> m;
+
+	for (int i = 0; i < n; i++) {
+			cin>>a[i];		
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+
+			if (a[i][j] == 'L') {
+				init();
+				bfs(i, j);
+			}
+			
+		}
+	}
+	cout << Max;
+	
+	return 0;
+}
